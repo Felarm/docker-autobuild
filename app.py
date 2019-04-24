b93582e2f05cefcafff137a6b477b7c88bbceac8
Felarm <kanafjev@gmail.com>
2019-04-24 14:20:38 +0300

    в app.py добавил скелет программы. Это рабочий вариант, однако как только количество запросов на скачивание образа из сети превышает количество воркеров, указанных при запуске приложения - вызовы начинают блокировать сервер, пока один из воркеров не освободится
    в app_async_test.py попытка реализовать скачивание образа через очередь queue.Queue

--- app.py
+++ app.py
@@ -4,38 +4,45 @@ from sanic import Sanic
 from sanic.response import json
 
 app = Sanic()
+NUM_WORKERS = 5
 
 
 @app.route('/start', methods=['POST'])
-async def run_container(request):
-    print(request)
+async def build_and_run_container(request):
     input_file = request.files.get('file')
-    print(input_file.name)
     with open(input_file.name, 'r') as ymlfile:
         docker_config = yaml.safe_load(ymlfile)
-    print(docker_config)
     for key, value in docker_config.items():
         dicti = docker_config[key]
     name = list(docker_config.keys())[0]
     run_params = dicti.get('properties')
-    image = run_params.get('image')
+    image = run_params.get('image')+':latest'
     command = run_params.get('command')
     ports = run_params.get('port_bindings')[0]
     client = docker.from_env()
-    build_image = client.images.pull(image+':latest') #скачивание образа, перенести в отдельный поток
-    print('pulled image')
-    cont = client.containers.run(name=name, image=build_image, command=command, ports=ports, detach=True)
-    print(cont.id)
-    print(client.containers.list())
-    cont.stop()
-    print('container ', cont.id, ' stopped')
-    cont.remove()
-    print(client.containers.list())
-    print(client.images.list())
-    client.images.remove(image=image)
-    print(client.images.list())
+    try:
+        builded_image = client.images.get(name=image)
+    except:
+        builded_image = client.images.pull(name=image)
+    cont = client.containers.run(name=name, image=builded_image, command=command, ports=ports, detach=True)
     return json({'container id': cont.id})
 
 
+@app.route('/stop/<int: id>', methods=['POST'])
+async def stop_container(request):
+    client = docker.from_env()
+    id = request.get('id')
+    cont = client.containers.get(id)
+    cont.stop()
+    cont.remove()
+    return json({'status': 'stoped and removed'})
+
+
+@app.route('/list', methods=['GET'])
+def list_containers():
+    client = docker.from_env()
+    return json({'containers': client.containers.list()})
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, workers=4)
+    app.run(host='0.0.0.0', port=8000, workers=NUM_WORKERS)
--- app_async_test.py
+++ app_async_test.py
@@ -2,24 +2,44 @@ import yaml
 import docker
 from sanic import Sanic
 from sanic.response import json
-import concurrent.futures
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing.pool import ThreadPool
+from queue import Queue
+from threading import Thread
 
 app = Sanic()
 
 
-async def build_container(image):
-    print('in build_container')
-    import json
-    cli = docker.APIClient()
-    #for line in cli.pull(image, stream=True, decode=True):
-    #    await print(line['status'])
-    cli.pull(image, stream=True, decode=True)
-    cli = docker.from_env()
-    return cli.images.get(name=image)
-        #print(json.dumps(line, indent=4)['status'])
+class TaskQueue(Queue):
 
+    def __init__(self, num_workers=1):
+        Queue.__init__(self)
+        self.num_workers = num_workers
+        self.start_workers()
+
+    def add_task(self, task, *args, **kwargs):
+        args = args or ()
+        kwargs = kwargs or {}
+        self.put((task, args, kwargs))
+
+    def start_workers(self):
+        for i in range(self.num_workers):
+            t = Thread(target=self.worker)
+            t.daemon = True
+            t.start()
+
+    def worker(self):
+        while True:
+            data = self.get()
+            func, args, kwargs = self.get()
+            image = func(*args, **kwargs)
+            self.task_done()
+            return image
+
+
+def pull_image_and_build(client, image, name, command, ports):
+    builded_image = client.images.pull(image)
+    cont = client.containers.run(name=name, image=builded_image, command=command, ports=ports, detach=True)
+    return json({'container id': cont.id})
 
 
 @app.route('/start', methods=['POST'])
@@ -38,24 +58,30 @@ async def run_container(request):
     try:
         print('in try')
         builded_image = client.images.get(name=image)
+        cont = client.containers.run(name=name, image=builded_image, command=command, ports=ports, detach=True)
+        return json({'container id': cont.id})
     except:
-        print('in exception')
-        builded_image = await build_container(image)
+        print('in except')
+        q.add_task(pull_image_and_build, (client, image,))
+        print('task added')
+        print(q.get()[0])
+        builded_image = q.get()
+        print(builded_image)
 
-    cont = client.containers.run(name=name, image=builded_image, command=command, ports=ports, detach=True)
 
-    print('container id: ', cont.id)
-    print(client.containers.list())
-    cont.stop()
-    print('container ', cont.id, ' stopped')
-    cont.remove()
-    print(client.containers.list())
-    print(client.images.list())
-    client.images.remove(image=image)
-    print(client.images.list())
-    return json({'container id': cont.id})
+
+    #print('container id: ', cont.id)
+    #print(client.containers.list())
+    #cont.stop()
+    #print('container ', cont.id, ' stopped')
+    #cont.remove()
+    #print(client.containers.list())
+    #print(client.images.list())
+    #client.images.remove(image=image)
+    #print(client.images.list())
 
 
 if __name__ == '__main__':
-    #pool = ThreadPoolExecutor()
-    app.run(host='0.0.0.0', port=8000, workers=4)
+    q = TaskQueue(num_workers=5)
+    app.run(host='0.0.0.0', port=8000, workers=2)
+    #app.create_server(host='0.0.0.0', port=8000)
